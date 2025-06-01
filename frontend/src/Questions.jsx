8ec74decd84395a7c1d4992bcd98299f0378be72
islome <islombekkamoliddinoff05@gmail.com>
2025-06-01 16:14:18 +0500

    fix: correct typo in API endpoint URL in Questions component

--- frontend/src/Questions.jsx
+++ frontend/src/Questions.jsx
@@ -11,7 +11,7 @@ function Questions() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    fetch('hhttp://51.21.224.147/api/questions')
+    fetch('http://51.21.224.147/api/questions')
       .then((res) => res.json())
       .then((data) => setQuestions(data))
       .catch((err) => console.error('Savollar yuklanmadi:', err));
